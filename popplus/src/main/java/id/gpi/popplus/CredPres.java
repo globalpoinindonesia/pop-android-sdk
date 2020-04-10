a0d1fb2caa13e8929aad1798a39fe576f7d94786
aignatd <ignat.deswanto@gpi.id>
2020-04-10 18:26:18 +0700

    Implement presenter

--- popplus/src/main/java/id/gpi/popplus/CredPres.java
+++ popplus/src/main/java/id/gpi/popplus/CredPres.java
@@ -1,12 +1,18 @@
 package id.gpi.popplus;
 
 import id.gpi.popplus.base.BaseViewPresenter;
+import id.gpi.popplus.service.POSLink;
 
-public class CredPres extends BaseViewPresenter {
+public class CredPres extends BaseViewPresenter implements Credentials.Presenter {
 	private static final String TAG = "Credentials";
 
 	public static String getToken()
 	{
 		return "Library berhasil";
+	}
+
+	@Override
+	public void getTokenSend(POSLink posLink) {
+
 	}
 }
