85191a429f3c98d54012f401f56abfc98f51799f
aignatd <ignat.deswanto@gpi.id>
2020-04-10 18:35:50 +0700

    Remove static string getToken

--- popplus/src/main/java/id/gpi/popplus/CredPres.java
+++ popplus/src/main/java/id/gpi/popplus/CredPres.java
@@ -1,14 +1,31 @@
 package id.gpi.popplus;
 
+import android.content.Context;
+import android.view.View;
+
 import id.gpi.popplus.base.BaseViewPresenter;
 import id.gpi.popplus.service.POSLink;
 
 public class CredPres extends BaseViewPresenter implements Credentials.Presenter {
 	private static final String TAG = "Credentials";
+	private Context context;
+	private Credentials.View view;
 
+/*
 	public static String getToken()
 	{
 		return "Library berhasil";
+	}
+*/
+
+	public CredPres(Context context, Credentials.View view) {
+		this.context = context;
+		this.view = view;
+	}
+
+	@Override
+	public void attachView(View view) {
+		super.attachView(view);
 	}
 
 	@Override
