4e4a24bb39970d387cfbdba7347654ed79ddab01
aignatd <ignat.deswanto@gpi.id>
2020-04-10 17:42:02 +0700

    Master first upload

---
+++ popplus/src/main/java/id/gpi/popplus/Credentials.java
@@ -0,0 +1,10 @@
+package id.gpi.popplus;
+
+public class Credentials {
+	private static final String TAG = "Credentials";
+
+	public static String getToken()
+	{
+		return "Library berhasil";
+	}
+}
