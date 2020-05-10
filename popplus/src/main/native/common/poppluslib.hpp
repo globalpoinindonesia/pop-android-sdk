135eaa170d6361959202d7a2233b47d961670ae7
aignatd <ignat.deswanto@gpi.id>
2020-05-10 22:32:57 +0700

    Change all key

--- popplus/src/main/native/common/poppluslib.hpp
+++ popplus/src/main/native/common/poppluslib.hpp
@@ -1,13 +1,13 @@
 #include <string>
 
 // development basic authentication
-std::string chrUserAuth("Q8hmGlanDbzf3UlXL/uN0YsEWwYJqYTUepzUk/iCXLk=");
-std::string chrPassAuth("fAy0bf7WB1ex3OsnzudNH4HYqgGH386mUMV4mLkuAas=");
+std::string chrUserAuth("a53eb0f72a3b2cadb5132ac99e6ea008");
+std::string chrPassAuth("520b6094f3203c4a4f0abfb3abf06b1a");
 
 // development encryption key
-std::string devkey("ziwz941qo9Ew7BAEPeex3vTxsACdQ/w5JG/p7L745OU=");
+std::string devkey("d53e2a00d411fa38c3205a0746f906dc");
 
 // development init vector
-std::string deviv("iZu8eCEejbjNbj3ixHba3R0xh33bINIGYfKOjOXcX7k=");
+std::string deviv("9404d6144f5cb537eb499d2097f791c5");
 
 const int BLOCK_SIZE = 32;
